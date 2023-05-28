f21531b72e859facd3405c6f447c3fa7d49daa77
Mikhailo Salko <slipknotmisha@gmail.com>
2023-05-28 11:10:25 +0300

    fisrt task lesson 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import SearchForm from "./components/SearchForm/SearchForm";
 import List from "./components/List/List";
@@ -6,87 +6,64 @@ import SearchTerm from "./components/SearchTerm/SearchTerm";
 import { StyledContainer, StyledTitle } from "./styles";
 import { fetchData } from "./Api";
 
-class App extends Component {
-  state = {
-    searchTerm: "",
-    stories: [],
-    isLoading: false,
-    isError: false,
-  };
+function App() {
+  const [searchTerm, setSearchTerm] = useState(localStorage.getItem("searchTerm") || "");
+  const [stories, setStories] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-  componentDidMount() {
-    const searchTerm = localStorage.getItem("searchTerm");
-    if (searchTerm) {
-      this.setState({ searchTerm });
-    };
-   this.fetchStories()
-  }
+  useEffect(() => {
+    fetchStories();
+    localStorage.setItem("searchTerm", searchTerm);
+  }, [searchTerm]);
 
-  componentDidUpdate(_, { searchTerm }) {
-    if (searchTerm !== this.state.searchTerm) {
-      localStorage.setItem("searchTerm", this.state.searchTerm);
-      this.fetchStories()
-    };
-  };
-
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
 
     const searchTerm = event.target.elements.search.value;
 
-    this.setState({ searchTerm });
+    setSearchTerm(searchTerm);
     event.target.reset();
   };
 
-  fetchStories = () => {
-    this.setState({ isLoading: true });
+  const fetchStories = () => {
+    setIsLoading(true);
 
-    fetchData(this.state.searchTerm).then(res => {
-      this.setState({ stories: res.hits })
-    }).catch((error) => {
-      this.setState({ isError: true })
-    }).finally(() => {
-      this.setState({ isLoading: false })
-    });
+    fetchData(searchTerm)
+      .then((res) => {
+        setStories([...res.hits]);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
 
-  // filterStories = () => {
-  //   return this.state.stories.filter((story) => {
-  //     if (!story.title) return true;    
-  //       return story.title.toLowerCase().includes(this.state.searchTerm.toLowerCase())
-  //   }
-      
-  //   );
-  // };
-
-  handleRemoveStory = (objectID) => {
-    this.setState((prevState) => ({
-      stories: prevState.stories.filter((story) => story.objectID !== objectID),
-    }));
+  const handleRemoveStory = (objectID) => {
+    setStories(() => stories.filter((story) => story.objectID !== objectID));
   };
 
-  resetSearchTerm = () => {
-    this.setState({ searchTerm: "" });
+  const resetSearchTerm = () => {
+    setSearchTerm("");
   };
 
-  render() {
-    const { searchTerm, isLoading, isError, stories } = this.state;
-    return (
-      <StyledContainer>
-        <div>
-          <a href="https://reactjs.org" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-          <StyledTitle>Hacker Stories</StyledTitle>
-        </div>
-        <SearchForm handleSubmit={this.handleSubmit} />
-        {searchTerm && <SearchTerm searchTerm={searchTerm} onClick={this.resetSearchTerm} />}
-        {isLoading && <p>Loading</p>}
-        {isError && <p>Something went wrong</p>}
-        <List stories={stories} handleRemoveStory={this.handleRemoveStory} />
-      </StyledContainer>
-    );
-  }
+  return (
+    <StyledContainer>
+      <div>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <StyledTitle>Hacker Stories</StyledTitle>
+      </div>
+      <SearchForm handleSubmit={handleSubmit} />
+      {searchTerm && <SearchTerm searchTerm={searchTerm} onClick={resetSearchTerm} />}
+      {isLoading && <p>Loading</p>}
+      {error && <p>Something went wrong</p>}
+      <List stories={stories} handleRemoveStory={handleRemoveStory} />
+    </StyledContainer>
+  );
 }
 
 export default App;
